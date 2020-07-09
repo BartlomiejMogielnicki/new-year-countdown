d7f938184f637565a79b72dd374c950c42c417ee
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-09 14:57:30 +0200

    Update DOM

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 
 const currentYear = new Date().getFullYear();
@@ -15,6 +15,11 @@ const updateCountdown = () => {
     const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
     const minutes = Math.floor(difference / 1000 / 60) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
+
+    daysEl.textContent = days;
+    hoursEl.textContent = hours < 10 ? `0${hours}` : hours;
+    minutesEl.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    secondsEl.textContent = seconds < 10 ? `0${seconds}` : seconds;
 };
 
 setInterval(updateCountdown, 1000);
