dc595456a11d757ecef509448613a50d83e6c0d6
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-09 15:15:54 +0200

    Add loading spinner and next year date

--- js/script.js
+++ js/script.js
@@ -4,22 +4,39 @@ const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+// Count years
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Update countdown timer and DOM
 const updateCountdown = () => {
     const currentTime = new Date();
     const difference = newYearTime - currentTime;
 
+    // Count timers
     const days = Math.floor(difference / 1000 / 60 / 60 / 24);
     const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
     const minutes = Math.floor(difference / 1000 / 60) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
 
-    daysEl.textContent = days;
-    hoursEl.textContent = hours < 10 ? `0${hours}` : hours;
-    minutesEl.textContent = minutes < 10 ? `0${minutes}` : minutes;
-    secondsEl.textContent = seconds < 10 ? `0${seconds}` : seconds;
+    // Update DOM
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours < 10 ? `0${hours}` : hours;
+    minutesEl.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    secondsEl.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 };
 
+// Update year
+year.innerHTML = currentYear + 1;
+
+// Show/hide loader and timers
+setTimeout(() => {
+    loading.remove();
+    countdown.classList.remove('hide');
+}, 1000);
+
+// Update time every 1 sec
 setInterval(updateCountdown, 1000);
